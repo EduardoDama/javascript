ed67765b13b86a201eed0e5e3793c0d7b0f0abdb
EduardoDama <102323022+EduardoDama@users.noreply.github.com>
2022-07-06 19:47:53 -0300

    atualizei

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 70){ //Condição simples
+    console.log('Você recebeu multa, ultrapassou o limite de 70km/h')
+}
+
+console.log('Sempre use o cinto de segurança!')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA'
+
+console.log(`você vive em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}
+else{
+    console.log('Você é estrangeiro!')
+}
