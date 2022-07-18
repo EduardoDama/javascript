17cadc102eb3cd06459ce1aa4ecf0ff2c0715288
Eduardo Damasceno <102323022+EduardoDama@users.noreply.github.com>
2022-07-18 16:09:36 -0300

    atualizei

---
+++ aulas/aula13/script.js
@@ -0,0 +1,21 @@
+/*
+console.log('olá')
+console.log('olá')
+console.log('olá')
+console.log('olá')
+console.log('olá')
+console.log('olá')
+*/
+
+
+/*var n1 = 1
+while(n1 <= 6){
+    console.log(`Número ${n1}`)
+    n1 ++
+}*/
+
+n1 = 1
+do {
+    console.log(`Número ${n1}`)
+    n1 ++
+} while (n1 <= 6);
---
+++ aulas/aula14/script.js
@@ -0,0 +1,5 @@
+console.log('vai começar...')
+for (c = 1; c <= 5; c++) {
+    console.log(c)    
+}
+console.log('FIM!')
