765fe9b2c67cd1af74620aba80eb18edc0987836
EduardoDama <102323022+EduardoDama@users.noreply.github.com>
2022-07-26 11:41:03 -0300

    atualizei

--- aulas/aula16ex/ex018/script.js
+++ aulas/aula16ex/ex018/script.js
@@ -1,12 +1,50 @@
+let nuns = []
+let soma = 0
+let maior = 0
+let menor = 0
 function adicionar() {
     let num = Number(document.getElementById('num').value) 
     let resp = document.getElementById('mostra')   
     let option = document.createElement('option')
 
-    let numbers = []
 
     option.text = `Valor ${num} adicionado`
-    numbers.push(num)
-    console.log(numbers.length)
+    nuns.push(num)
+    soma += num
+
+    if(maior == 0){
+        maior = num
+        menor = num
+    }else{
+        if(num > maior){
+            maior = num
+        }
+        if(num < menor){
+            menor = num
+        } 
+    }
+
+
+
     resp.appendChild(option)
+}
+
+function calc() {
+    let div = document.createElement('div')
+    let resp = document.getElementById('resp')
+
+    let media = soma / nuns.length
+
+    div.innerHTML = `<p>Você adicionou ${nuns.length} Números</p>`
+    resp.appendChild(div)
+    div.innerHTML = `<p>a soma de todos os valores foi ${soma}</p>`
+    resp.appendChild(div)
+    div.innerHTML = `<p>o maior número informado foi ${maior}</p>`
+    resp.appendChild(div)
+    div.innerHTML = `<p>o menor número informado foi ${menor}</p>`
+    resp.appendChild(div)
+    div.innerHTML = `<p>A média dos valores informados foi ${media.toFixed(2).replace('.',',')}</p>`
+
+    resp.appendChild(div)
+    
 }
