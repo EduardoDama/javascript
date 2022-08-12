9475791ec43d0edc13813e8ce83ec8e49dcc49e4
EduardoDama <102323022+EduardoDama@users.noreply.github.com>
2022-08-12 20:46:05 -0300

    atualizado

---
+++ exercicio/uol guerra Ucrânia e Russia/script.js
@@ -0,0 +1,50 @@
+function vd1(){
+    let div = document.createElement('div')
+    let resp = document.getElementById('resp')
+
+    resp.innerHTML = ''
+
+    div.setAttribute('id', 'v')
+
+    div.innerHTML = '<iframe width="904" height="509" src="https://www.youtube.com/embed/cXA-AFLNio4" title="E se atacassem uma usina nuclear? - Brasil Escola" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+    resp.appendChild(div)
+
+}
+function vd2(){
+   
+    let div = document.createElement('div')
+    let resp = document.getElementById('resp')
+    resp.innerHTML = '' 
+    div.setAttribute('id', 'v')
+
+
+    div.innerHTML = '<iframe width="904" height="509" src="https://www.youtube.com/embed/AfBL9d-vsUo" title="Ucrânia - Brasil Escola" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+    resp.appendChild(div)
+
+}
+function vd3(){
+    let div = document.createElement('div')
+    let resp = document.getElementById('resp')
+    resp.innerHTML = ''
+    div.setAttribute('id', 'v')
+
+
+    div.innerHTML = '<iframe width="904" height="509" src="https://www.youtube.com/embed/hahAmVoqGsc" title="Guerra na Ucrânia - Brasil Escola" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+    resp.appendChild(div)
+
+}
+function vd4(){
+    let div = document.createElement('div')
+    let resp = document.getElementById('resp')
+    resp.innerHTML = ''
+    div.setAttribute('id', 'v')
+
+
+    div.innerHTML = '<iframe width="904" height="509" src="https://www.youtube.com/embed/5hmMxxoR5i4" title="Tensão entre Rússia e Ucrânia - Brasil Escola" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+    resp.appendChild(div)
+
+}
